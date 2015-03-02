9ac4ee737b36e95651e978ea7f5333b5409eafbd
KoeSeph <koesavant@gmail.com>
2015-03-02 12:44:05 -0600

    cleaning up
    code

---
+++ static/loader.js
@@ -0,0 +1,17 @@
+  $(document).ready(function() {
+    $('#thumbnail-fine-uploader').fineUploader({
+      template: "qq-simple-thumbnails-template",
+      thumbnails: {
+        placeholders: {
+          waitingPath: "waiting-generic.png",
+          notAvailablePath: "not_available-generic.png"
+        }
+      },
+      request: {
+        endpoint: '/upload'
+      },
+      validation: {
+        allowedExtensions: ['jpeg', 'jpg', 'gif', 'png']
+      }
+    });
+  });
